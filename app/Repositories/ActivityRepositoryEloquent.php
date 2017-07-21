bf741aa2bdab4aae947cad79e10c45c557ec8c9e
João Sales <joaosalless@gmail.com>
2017-07-21 11:24:25 -0300

    implementados métodos obrigatórios rulesCreating() e rulesUpdating()  nos repositórios

--- app/Repositories/ActivityRepositoryEloquent.php
+++ app/Repositories/ActivityRepositoryEloquent.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Contracts\ModelContract;
 use App\Models\Activity;
 use App\Repositories\ActivityRepository;
 
@@ -12,6 +13,28 @@ use App\Repositories\ActivityRepository;
  */
 class ActivityRepositoryEloquent extends AbstractRepositoryEloquent implements ActivityRepository
 {
+    /**
+     * Specify Model Create rules
+     *
+     * @return array
+     */
+    public function rulesCreating(): array
+    {
+        return [];
+    }
+
+    /**
+     * Specify Model Update rules
+     *
+     * @param ModelContract $model
+     *
+     * @return array
+     */
+    public function rulesUpdating(ModelContract $model = null): array
+    {
+        return [];
+    }
+
     /**
      * Specify Model class name
      *
--- app/Repositories/UserRepositoryEloquent.php
+++ app/Repositories/UserRepositoryEloquent.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Contracts\ModelContract;
 use App\Models\User;
 use App\Repositories\UserRepository;
 
@@ -12,6 +13,28 @@ use App\Repositories\UserRepository;
  */
 class UserRepositoryEloquent extends AbstractRepositoryEloquent implements UserRepository
 {
+    /**
+     * Specify Model Create rules
+     *
+     * @return array
+     */
+    public function rulesCreating(): array
+    {
+        return [];
+    }
+
+    /**
+     * Specify Model Update rules
+     *
+     * @param ModelContract $model
+     *
+     * @return array
+     */
+    public function rulesUpdating(ModelContract $model = null): array
+    {
+        return [];
+    }
+
     /**
      * Specify Model class name
      *
